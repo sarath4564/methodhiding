06d0cefe9f92dd4b65080cffa2a29029b3762019
Sarath Chandra K <sarathchandrakroyal@gmail.com>
2022-07-21 17:09:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+// 'new' modifier will hide the base class implementation
+namespace Demo
+{
+    class Employee
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public void Print()
+        {
+            Console.WriteLine($"{FirstName}{LastName}");
+        }
+    }
+    class Developer:Employee
+    {
+        public new void Print()   // hides the base class implementation
+        {
+            Console.WriteLine($"{FirstName}{LastName}Decveloper");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Developer developer = new Developer { FirstName="Sarath",LastName="Chandra"};
+            developer.Print();
+        }
+    }
+}
